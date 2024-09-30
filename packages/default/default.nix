1dc45b4e01bc9cd84d96727f2bdfa148d651cf43
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-10-01 01:25:45 +0300

    build: Pin Rust version to `1.81.0`, fix `nix` package build

--- packages/default/default.nix
+++ packages/default/default.nix
@@ -7,7 +7,7 @@
 let
     toolchain = (pkgs.rustChannelOf {
         rustToolchain = ../../rust-toolchain.toml;
-        sha256 = "3jVIIf5XPnUU1CRaTyAiO0XHVbJl12MSx3eucTXCjtE=";
+        sha256 = "VZZnlyP69+Y3crrLHQyJirqlHrTtGTsyiSnZB8jEvVo=";
     }).rust;
 
     naersk' = pkgs.callPackage inputs.naersk {
